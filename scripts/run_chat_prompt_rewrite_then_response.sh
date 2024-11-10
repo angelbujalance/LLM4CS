8db4462825251215be949b39982df083a3fe9a91
angelbujalance <angelbujalanceg@gmail.com>
2024-11-10 21:31:03 +0100

    RTR for cast19

--- scripts/run_chat_prompt_rewrite_then_response.sh
+++ scripts/run_chat_prompt_rewrite_then_response.sh
@@ -5,9 +5,9 @@
 #SBATCH --job-name=RTR
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=18
-#SBATCH --time=01:00:00
-#SBATCH --output=output/success/out-RTR_prompting.out
-#SBATCH --error=output/error/out-RTR_prompting.err
+#SBATCH --time=10:00:00
+#SBATCH --output=output/success/out-RTR_prompting_TEST.out
+#SBATCH --error=output/error/out-RTR_prompting_TEST.err
 
 python chat_prompt_rewrite_then_response.py \
 --open_ai_key_id=0 \
@@ -15,5 +15,5 @@ python chat_prompt_rewrite_then_response.py \
 --qrel_file_path="./datasets/cast19_qrel.tsv" \
 --work_dir="./results/new/cast19/RTR" \
 --demo_file_path="./demonstrations.json" \
---rewrite_file_path="./results/new/cast19/RTR/rewrites.jsonl" \
+--rewrite_file_path="./results/new/cast19/REW/rewrites.jsonl" \
 --n_generation=5 \
