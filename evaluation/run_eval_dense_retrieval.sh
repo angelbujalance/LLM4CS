06ab37139b4a5dc0174bddb625964fea34fcf39d
anesaibr <anesaibrahimi26@gmail.com>
2024-11-25 13:35:23 +0100

    evaluation script/job file

--- evaluation/run_eval_dense_retrieval.sh
+++ evaluation/run_eval_dense_retrieval.sh
@@ -1,25 +1,52 @@
-# eval_dense_retrieval.sh
-# An example of evaluating RTR on cast20.
-eval_field_name="predicted_rewrite"
-work_dir="../results/cast20/RTR"    # set your the folder containing your `rewrites.jsonl`file
+#!/bin/bash
 
-eval_file_path="$work_dir/rewrites.jsonl" \
-index_path="" # set the pre-built index which contains all candidate passage emebddings. 
-qrel_file_path="../datasets/cast20_qrel.tsv" # set the qrel file path
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --job-name=Evaluation_llmcs2_msmarco_new_embs_org
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --time=01:00:00
+#SBATCH --output=output/success/out-eval_dense_%x.%A.out
+#SBATCH --error=output/error/out-eval_dense_%x.%A.err
+
+module purge
+module load 2022
+module load Anaconda3/2022.05
+
+# Initialize Conda
+source /sw/arch/RHEL8/EB_production/2022/software/Anaconda3/2022.05/etc/profile.d/conda.sh
+
+# Activate the correct Conda environment
+conda activate llmcs2
+
+
+# Change to the evaluation directory
+cd $HOME/LLM4CS/evaluation
+
+
+eval_field_name="predicted_rewrite"
+work_dir="../results/new/cast19/REW"    # set your the folder containing your `rewrites.jsonl`file
+
+eval_file_path="$work_dir/rewrites.jsonl" 
+index_path="/scratch-shared/llm4cs/new_embeddings/" # set the pre-built index which contains all candidate passage emebddings. 
+qrel_file_path="../datasets/cast19_qrel.tsv" # set the qrel file path
 retrieval_output_path="$work_dir/ance/+q+r+sc" # set your expected output folder path
 
+#----------(disabled)-----------------------------------
+# --include_response \ # enable `include_response` if you test RTR or RAR prompting.
+# --use_gpu_in_faiss \
+# --n_gpu_for_faiss=1 \
+#---------------------------------------------                                                                                   
 export CUDA_VISIBLE_DEVICES=0
-python eval_dense_retrieval.py \
+# python eval_dense_retrieval.py \
+python wrapper_eval_dense_retrieval.py \
 --eval_file_path=$eval_file_path \
 --eval_field_name=$eval_field_name \
 --qrel_file_path=$qrel_file_path \
 --index_path=$index_path \
---retriever_path="" \ # set the pre-trained ANCE model path.
---use_gpu_in_faiss \
---n_gpu_for_faiss=1 \
+--retriever_path="/scratch-shared/ad-hoc-ance-msmarco/" \
 --top_n=1000 \
---rel_threshold=2 \ # set 1 for cast19 and 2 for cast20.
+--rel_threshold=1 \
 --retrieval_output_path=$retrieval_output_path \
 --include_query \
---include_response \ # enable `include_response` if you test RTR or RAR prompting.
---aggregation_method="sc" \ # you can set [`maxprob, mean, sc`]
+--aggregation_method="sc"
